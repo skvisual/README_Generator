543ace6d2ea5d13745516ff66cc67dd1fdac5ef5
skvisual <southkvisual@gmail.com>
2020-03-25 00:30:12 -0500

    updated index.js, created questions array

--- index.js
+++ index.js
@@ -4,54 +4,71 @@ var inquirer = require('inquirer')
 const apikey = '4efea0fa4c2aad2d02260be69fb7ed026869eec6'
 // var username = "username"
 
-// const questions = [
-
-// ];
+const questions = [
+  {
+    type: 'input',
+    message: 'Enter your GitHub username:',
+    name: 'username'
+  },
+  {
+    type: 'input',
+    message: 'Enter a project title:',
+    name: 'projectName'
+  },
+  {
+    type: 'input',
+    message: 'Add a description of your project',
+    name: 'projectDesc'
+  },
+  {
+    type: 'confirm',
+    message: 'Do you want to make a ToC now?',
+    name: 'tableConfirm'
+  },
+];
 
 inquirer
-  .prompt([
-    {
-      message: 'Enter your GitHub username:',
-      name: 'username'
-    },
-    {
-      message: 'Enter a project title:',
-      name: 'projectName'
-    },
-    {
-      message: 'Add a description of your project',
-      name: 'projectDesc'
-    },
-  ])
-  .then(function({ username, projectName, projectDesc }) {
-    console.log(username)
-    console.log(projectName)
-    //Append [rojectName to 'README.md' using fs method appendFile(), make large by using #? fs.appendFile('README.md', '#' + res.projectName, function(err){}); 
+  .prompt(questions)
+  .then(function({ username, projectName, projectDesc, tableConfirm}) {
+    //Define queryURL with const. use ${username}
+    const queryURL = `https://api.github.com/users/${username}`;
+
+    axios
+    .get(queryURL) //use queryURL defined above
+    .then(function(res) { //run function with the response data as a parameter
+        // console.log(res);
+        console.log(res.data.avatar_url);
+      
+        console.log(res.data.email);
+    });
+    // console.log(username)
+    // console.log(projectName)
+
+    //Append projectName to 'README.md' using fs method appendFile(), make large by using #? fs.appendFile('README.md', '#' + res.projectName, function(err){}); 
     fs.appendFile('README.md', '# ' + projectName + `\n`, function(err){
       if (err) {
         console.log('error')
       }else
-        console.log('success')
+        console.log('append projectName success')
     });
-    console.log(projectDesc) 
+
+    // console.log(projectDesc) 
+
     //Append projectDesc to 'README.md' using fs method appendFile()
     fs.appendFile('README.md', projectDesc + `\n`, function(err){
       if (err) {
         console.log('error')
       }else
-        console.log('success')
+        console.log('append projectDesc success')
     });
-    //Define queryURL with const. use ${username} and ${apikey} to dynamically insert values to API URL.
-    const queryURL = `https://api.github.com/users/${username}/repos?per_page=100?apikey=${apikey}`;
-
-    axios
-    .get(queryURL) //use queryURL defined above
-    .then(function(res) { //run function with the response data as a parameter
-        console.log(res);
-        // console.log(res.data[0].owner.avatar_url); //this logs the url of GitHub profile picture. HOW TO USE AS AN IMG?
-       
     
-    })
+
+    if (tableConfirm !== true) {
+      console.log("go to next question")
+    } else {
+      console.log("prompt for header or subtext")
+    }
+    
   });
 
 
